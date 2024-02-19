f577003bfa3642fbdf564476dd95276df2a2d720
Nickita van Noort <vani23du@student.ju.se>
2024-02-19 09:05:31 +0100

    Adding more.

--- lunar-lander-assignment/lunar-lander.js
+++ lunar-lander-assignment/lunar-lander.js
@@ -1,22 +1,20 @@
 //Global
+let fuel = 100;
+let spacecraftX = 400;
+let state = "start";
+let timer = 0;
+textFont("ITC Avant Garde Gothic Pro");
 
 //Setup
 function setup() {
   createCanvas(800, 700);
-}
-
-//Draw
-function draw() {
   background(235, 181, 191);
-  planet();
-  spacecraft();
-  carrot();
 }
 
-//Functions for each object
+//All Draw Functions
 function planet() {
-  fill(161, 194, 151);
   noStroke();
+  fill(161, 194, 151);
   rect(0, 500, 800, 200);
 }
 
@@ -49,11 +47,79 @@ function carrot() {
 function spacecraft() {
   fill(255, 255, 255);
 
-  //Body
-  rect(400, 100, 60, 80);
-  triangle(400, 100, 430, 70, 460, 100);
-  bezier(410, 170, 400, 130, 380, 190, 400, 200);
-  bezier(450, 170, 460, 130, 480, 190, 460, 200);
+  //The following six lines i got some help from ChatGPT. I did it mostly myself but got some help with the spacecraftX part.
+  rect(spacecraftX, 100, 60, 80);
+  triangle(spacecraftX, 100, spacecraftX + 30, 70, spacecraftX + 60, 100);
+  bezier(
+    spacecraftX + 10,
+    170,
+    spacecraftX,
+    130,
+    spacecraftX - 20,
+    190,
+    spacecraftX,
+    200
+  );
+  bezier(
+    spacecraftX + 50,
+    170,
+    spacecraftX + 60,
+    130,
+    spacecraftX + 80,
+    190,
+    spacecraftX + 60,
+    200
+  );
   fill(161, 194, 151);
-  rect(410, 110, 40, 30, 10);
+  rect(spacecraftX + 10, 110, 40, 30, 10);
+}
+
+/*function keyPressed() {
+  if (keyIsDown(LEFT_ARROW)) {
+    moveSpacecraft(-moveSpeed); // Move spacecraft left
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    moveSpacecraft(moveSpeed); // Move spacecraft right
+  }
+}*/
+
+//Screens Setup
+function screenStart() {
+  stroke("white");
+  strokeWeight(5);
+  fill(161, 194, 151);
+  rect(250, 150, 330, 100, 20);
+  fill(255);
+  strokeWeight(2);
+  textSize(30);
+  text("Letz Get Startet", 307, 210);
+  planet();
+  carrot();
+}
+
+function screenGame() {
+  text("Bunny Lander", 400, 350);
+}
+
+function screenResult() {
+  text("Results", 390, 400);
+}
+
+//Draw
+function draw() {
+  //Starter Screen
+  if (state === "start") {
+    screenStart();
+  }
+  //Game Screen
+  else if (state === "Bunny Lander") {
+    screenGame();
+    planet();
+    carrot();
+    spacecraft();
+    keyPressed();
+  }
+  //Result Screen
+  else if (state === "Results") {
+    screenResult();
+  }
 }
